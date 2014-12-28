012087de48b989b404c72708854d7ad7f0d33507
Jd Dantes <jddantes@gmail.com>
2014-12-28 12:18:05 +0800

    Reached slug in ch 7

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from rango.models import Category, Page
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name',)}
+	list_display = ('name', 'views', 'likes', 'slug')
+
+admin.site.register(Category, CategoryAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'url')
+
+admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 # Create your views here.
 
 def index(request):
-	context_dict = {'boldmessage': "I am bold font from the context"}
+	category_list = Category.objects.order_by('-likes')[:5]
+
+	context_dict = {'boldmessage': "I am bold font from the context", 'categories':category_list}
 	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
