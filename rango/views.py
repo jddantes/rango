e10b04f68b2e1d818b475e871c188b0e40f4426d
Jd Dantes <jddantes@gmail.com>
2015-04-25 15:13:57 +0800

    Done with chapter 8

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, PageForm
 
 # Create your views here.
 
@@ -20,6 +20,7 @@ def category(request, category_name_slug):
 		pages = Page.objects.filter(category=category)
 		context_dict['pages'] = pages
 		context_dict['category'] = category
+		context_dict['category_name_slug'] = category_name_slug
 	except:
 		pass
 
@@ -45,4 +46,28 @@ def add_category(request):
 
 	return render(request, 'rango/add_category.html', {'form': form})
 
+def add_page(request, category_name_slug):
 
+	try:
+		cat = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		cat = None
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if cat:
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+
+				return category(request, category_name_slug)
+			else:
+				print (form.errors)
+	else:
+		form = PageForm()
+
+	context_dict = {'form':form, 'category': cat}
+
+	return render(request, 'rango/add_page.html', context_dict)
