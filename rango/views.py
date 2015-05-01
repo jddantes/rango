a8026658d8caada46e511eeeaca31232a5f3df92
Jd Dantes <jddantes@gmail.com>
2015-05-01 21:46:36 +0800

    Done with ch 11

--- rango/views.py
+++ rango/views.py
@@ -5,8 +5,7 @@ from rango.forms import CategoryForm, PageForm
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
-
+from datetime import datetime
 
 # Create your views here.
 
@@ -14,7 +13,31 @@ def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'boldmessage': "I am bold font from the context", 'categories':category_list, 'pages':page_list}
-	return render(request, 'rango/index.html', context_dict)
+	
+	visits = request.session.get('visits')
+
+	if not visits:
+		visits = 1
+
+	reset_last_visit_time = False
+
+	last_visit = request.session.get('last_visit')
+	if last_visit:
+		last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+		if (datetime.now() - last_visit_time).seconds > 0:
+			visits = visits + 1
+			reset_last_visit_time = True
+	else:
+		reset_last_visit_time = True
+
+	if reset_last_visit_time:
+		request.session['last_visit'] = str(datetime.now())
+		request.session['visits'] = visits
+	context_dict['visits'] = visits
+
+	response = render(request, 'rango/index.html', context_dict)
+
+	return response
 
 def category(request, category_name_slug):
 	context_dict = {}
@@ -32,7 +55,46 @@ def category(request, category_name_slug):
 	return render(request, 'rango/category.html', context_dict)
 
 def about(request):
-	return HttpResponse("Rango says here is the about page")
+
+	visits = request.session.get('visits')
+	reset_last_visit_time = False
+	if not visits:
+		visits = 1
+
+	last_visit = request.session.get('last_visit')
+	if last_visit:
+		last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+		if(datetime.now() - last_visit_time).seconds > 0:
+			visits = visits + 1
+			reset_last_visit_time = True
+	else:
+		reset_last_visit_time = True
+
+	if reset_last_visit_time:
+		request.session['last_visit'] = str(datetime.now())
+		request.session['visits'] = visits
+
+	about_visits = request.session.get('about_visits')
+	reset_last_visit_time = False
+	if not about_visits:
+		about_visits = 1
+	last_about_visit = request.session.get('last_about_visit')
+	if last_about_visit:
+		last_about_visit_time = datetime.strptime(last_about_visit[:-7], "%Y-%m-%d %H:%M:%S")
+		if(datetime.now() - last_about_visit_time).seconds > 0:
+			about_visits = about_visits + 1
+			reset_last_visit_time = True
+	else:
+		reset_last_visit_time = True
+
+	if reset_last_visit_time:
+		request.session['about_visits'] = about_visits
+		request.session['last_about_visit'] = str(datetime.now())
+	
+	context_dict = {'visits': visits, 'about_visits':about_visits, }
+
+
+	return render(request, 'rango/about.html', context_dict)
 
 @login_required
 def add_category(request):
@@ -80,6 +142,7 @@ def add_page(request, category_name_slug):
 	return render(request, 'rango/add_page.html', context_dict)
 
 def register(request):
+
 	registered = False
 
 	if request.method == 'POST':
