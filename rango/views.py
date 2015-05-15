949974e50b4b8e228e9a95a5d8690153e1daea00
Jd Dantes <jddantes@gmail.com>
2015-05-16 00:12:47 +0800

    Done with 16.1: added page view tracker via get and redirect

--- rango/views.py
+++ rango/views.py
@@ -220,3 +220,20 @@ def search(request):
 			result_list = run_query(query)
 
 	return render(request, 'rango/search.html', {'result_list': result_list})
+
+def track_url(request):
+	if request.method == 'GET':
+		if 'page_id' in request.GET:
+			page_id = request.GET['page_id']
+			try:
+				pages = Page.objects.filter(url=page_id)
+				for page in pages:
+					page.views  = page.views+1
+					page.save()
+				return HttpResponseRedirect(page_id)
+			except Exception as e:
+				print('Didnt get object with url ' + page_id)
+				print(e)
+				pass
+	
+	return HttpResponseRedirect('/rango/')
